4ba929afb74da9965acdafc0b9c537cc8575a12a
Noneeman <94176544+noneeman@users.noreply.github.com>
2023-02-06 20:39:56 +0000

    Clickable name to return to home page

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function navBar(target) {
         projects.style.textDecoration = 'underline';
         experience.style.textDecoration = 'none';
         homePage.style.visibility = 'hidden';
-        projectsPage.style.visibility = 'visible';
         experiencePage.style.visibility = 'hidden';
+        projectsPage.style.visibility = 'visible';
         
     // if the button experience is clicked
     } else if(target.classList.contains('experience-btn')) {
@@ -24,13 +24,13 @@ function navBar(target) {
     projectsPage.style.visibility = 'hidden';
     experiencePage.style.visibility = 'visible';
     
-    // if the button home is clicked
-    } else {
+    // if the button home or name is clicked
+} else {
         home.style.textDecoration = 'underline';
         projects.style.textDecoration = 'none';
         experience.style.textDecoration = 'none';
-        homePage.style.visibility = 'visible';
         projectsPage.style.visibility = 'hidden';
         experiencePage.style.visibility = 'hidden';
+        homePage.style.visibility = 'visible';
     }
 };
